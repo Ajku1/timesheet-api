7892ad6a664da8f43ce3c1d6f7095c808a87bef8
Nikola Kostadinov <nikola_sk@abv.bg>
2022-06-12 12:27:04 +0300

    Add TimeEntryController

---
+++ Controllers/TimeEntryController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using timesheet_api.Data;
+using timesheet_api.Data.Entities.TimeEntry;
+
+namespace timesheet_api.Controllers;
+
+[ApiController]
+[Route("api/time-entries")]
+public class TimeEntryController : ControllerBase
+{
+    private readonly ITimesheetRepository _repository;
+
+    public TimeEntryController(ITimesheetRepository repository)
+    {
+        _repository = repository;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<TimeEntry>> GetTimeEntriesPendingReview(int managerId)
+    {
+        var timeEntries = _repository.GetTimeEntriesPendingReview(managerId);
+        return Ok(timeEntries);
+    }
+}
